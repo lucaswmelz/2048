62f24ac7564b58aa14c4dc15700e4230cea468ae
lucaswmelz <55547671+lucaswmelz@users.noreply.github.com>
2019-12-11 21:35:47 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-
 int geranumero()
 {
     int a = rand()%4;
@@ -15,7 +13,34 @@ int geranumero()
         return 4;
 }
 
-int baixo (int grelha[][10], int sz)
+void inicializagrelha(int grelha[][10], int sz)
+{
+    int linha0 = rand()%sz;
+    int coluna0 = rand()%sz;
+    for (int r=0; r<sz; r++) // preenche a matriz com 0's
+    {
+        for (int t=0; t<sz; t++)
+        {
+            grelha[r][t]=0;
+        }
+
+    }
+
+    grelha[linha0][coluna0] = geranumero();
+
+    int linha1 = rand()%sz;
+    int coluna1 = rand()%sz;
+    while (linha0==linha1 && coluna0==coluna1)
+    {
+        linha1 = rand()%sz;
+        coluna1 = rand()%sz;
+    }
+    grelha[linha1][coluna1] = geranumero();
+
+
+}
+
+/*int baixo (int grelha[][10], int sz)
 {
 
     for (int i=sz; i>0; i--)
@@ -35,7 +60,7 @@ int baixo (int grelha[][10], int sz)
         }
     }
 }
-
+*/
 
 int main()
 {
@@ -43,35 +68,10 @@ int main()
     printf("Qual a dimensao da grelha?\n");
     scanf("%d", &sz);
     srand(time(NULL));
-    int grelha[sz][sz];
+    int grelha[10][10];
+    inicializagrelha(grelha, sz);
 
-    //inicializando a grelha
-   int linha0 = rand()%sz;
-    int coluna0 = rand()%sz;
-    for (int r=0; r<sz; r++) // preenche a matriz com 0's
-    {
-        for (int t=0; t<sz; t++)
-        {
-            grelha[r][t]=0;
-        }
-
-    }
-
-    grelha[linha0][coluna0] = geranumero();
-
-    int linha1 = rand()%sz;
-    int coluna1 = rand()%sz;
-    while (linha0==linha1 && coluna0==coluna1)
-    {
-        int linha1 = rand()%sz;
-        int coluna1 = rand()%sz;
-    }
-    grelha[linha1][coluna1] = geranumero();
-    // termina de inicializar a grelha
-
-
-
-    for (int r=0; r<sz; r++) //imprime a matriz
+     for (int r=0; r<sz; r++) //imprime a matriz
     {
         for (int t=0; t<sz; t++)
         {
@@ -80,4 +80,6 @@ int main()
         printf("\n");
     }
 
+
+return 0;
 }
