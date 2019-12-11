e32fda5b1cead3dbdfa78561dab0a275fb7d8f6a
lucaswmelz <55547671+lucaswmelz@users.noreply.github.com>
2019-12-11 19:42:36 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -2,31 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
-int baixo (int grelha[][], int sz)
-{
-
-for (i=sz;i>0;i--):
-    {
-     for(j=0;j<sz;j++)
-       {
-        if (grelha[i][j]==grelha[i-1][j])
-          {
-            grelha[i][j]=grelha[i][j]*2;
-             if(i-2<0)
-               {
-                grelha[i-1][j]=0;
-               }
-              else
-                grelha[i-1][j]=grelha[i-2][j];
-           }
-        }
-      }
-}
 
 
 int geranumero()
 {
-int a = rand()%3;
+    int a = rand()%4;
     if(a<2)
     {
         return 2;
@@ -35,29 +15,69 @@ int a = rand()%3;
         return 4;
 }
 
+int baixo (int grelha[][10], int sz)
+{
+
+    for (int i=sz; i>0; i--)
+    {
+        for(int j=0; j<sz; j++)
+        {
+            if (grelha[i][j]==grelha[i-1][j])
+            {
+                grelha[i][j]=grelha[i][j]*2;
+                if(i-2<0)
+                {
+                    grelha[i-1][j]=0;
+                }
+                else
+                    grelha[i-1][j]=grelha[i-2][j];
+            }
+        }
+    }
+}
+
+
 int main()
 {
-int sz;
-printf("Qual a dimensao da grelha?\n");
-scanf("%d", &sz);
-srand(time(NULL));
-int grelha[sz][sz];
-for(int i=0; i<sz; i++)
-{
-    for(int j=0; j<sz; j++)
+    int sz;
+    printf("Qual a dimensao da grelha?\n");
+    scanf("%d", &sz);
+    srand(time(NULL));
+    int grelha[sz][sz];
+
+    //inicializando a grelha
+   int linha0 = rand()%sz;
+    int coluna0 = rand()%sz;
+    for (int r=0; r<sz; r++) // preenche a matriz com 0's
     {
-        grelha[i][j]=geranumero();
+        for (int t=0; t<sz; t++)
+        {
+            grelha[r][t]=0;
+        }
+
     }
 
-}
+    grelha[linha0][coluna0] = geranumero();
 
-for (int r=0; r<sz; r++)
-{
-    for (int t=0; t<sz; t++)
+    int linha1 = rand()%sz;
+    int coluna1 = rand()%sz;
+    while (linha0==linha1 && coluna0==coluna1)
     {
-        printf("%2d ", grelha[r][t]);
+        int linha1 = rand()%sz;
+        int coluna1 = rand()%sz;
     }
-    printf("\n");
-}
+    grelha[linha1][coluna1] = geranumero();
+    // termina de inicializar a grelha
+
+
+
+    for (int r=0; r<sz; r++) //imprime a matriz
+    {
+        for (int t=0; t<sz; t++)
+        {
+            printf("%2d", grelha[r][t]);
+        }
+        printf("\n");
+    }
 
 }
