1dda14d82e5dfe562548fd539bdc70d83bb0df5a
lucaswmelz <55547671+lucaswmelz@users.noreply.github.com>
2019-12-19 20:05:41 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -96,6 +96,61 @@ int direita (int grelha[][10], int sz)
     return numerodecombinacoes;
 }
 
+int esquerda (int grelha[][10], int sz)
+{
+    int numerodecombinacoes;
+    int i, j, h;
+    int count = 0;
+
+    /* joga os numeros para a esquerda se houver posições livres (com 0's) */
+
+    for (i=0; i<sz; i++)
+    {
+        for(j=1; j<sz; j++)
+        {
+            if(grelha[i][j]!=0)
+            {
+                for(h=j-1; h>=0; h--)
+                {
+                    if(grelha[i][h]==0)
+                    {
+                        count++;
+                    }
+                }
+                if(count>0)
+                {
+                    grelha[i][j-count]=grelha[i][j];
+                    grelha[i][j]=0;
+                }
+            }
+            count = 0;
+        }
+    }
+
+    /* soma os numeros iguais */
+
+    for (i=0; i<sz; i++)
+    {
+        for(j=0; j<sz-1; j++)
+        {
+            if(grelha[i][j]==grelha[i][j+1])
+            {
+                grelha[i][j]=grelha[i][j]*2;
+                grelha[i][j+1]=0;
+                numerodecombinacoes++;
+            }
+            if(grelha[i][j]==0)
+            {
+                grelha[i][j]=grelha[i][j+1];
+                grelha[i][j+1]=0;
+            }
+
+        }
+    }
+
+
+    return numerodecombinacoes;
+}
 
 int baixo (int grelha[][10], int sz)
 {
@@ -151,6 +206,60 @@ int baixo (int grelha[][10], int sz)
     return numerodecombinacoes;
 }
 
+int cima (int grelha[][10], int sz)
+{
+    int numerodecombinacoes = 0;
+    int count = 0;
+    int i, j, h;
+
+    for(i=1; i<sz; i++)
+    {
+        for (j=0; j<sz; j++)
+        {
+            if(grelha[i][j]!=0)
+            {
+                for(h=i-1; h>=0; h--)
+                {
+                    if(grelha[h][j]==0)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count>0)
+                {
+                    grelha[i-count][j]=grelha[i][j];
+                    grelha[i][j]=0;
+                }
+            }
+            count = 0;
+
+
+        }
+    }
+
+    for (i=0; i<sz-1; i++)
+    {
+        for(j=0; j<sz; j++)
+        {
+            if(grelha[i][j]==grelha[i+1][j])
+            {
+                grelha[i][j]=grelha[i][j]*2;
+                grelha[i+1][j]=0;
+                numerodecombinacoes++;
+            }
+            if(grelha[i][j]==0)
+            {
+                grelha[i][j]=grelha[i+1][j];
+                grelha[i+1][j]=0;
+            }
+
+        }
+    }
+
+    return numerodecombinacoes;
+}
+
 void mostrar (int grelha[][10], int sz)
 {
     for (int r=0; r<sz; r++) //imprime a matriz
@@ -186,20 +295,48 @@ int main()
     srand(time(NULL));
     int grelha[10][10];
     inicializagrelha(grelha, sz);
-    int testa=1;
+    char aux = 'C';
+    char aux2 = 'D';
+    char aux3 = 'B';
+    char aux4 = 'E';
+    char aux5 = 'C';
+    int jogada = (int)aux;
+    int numero_de_comb = 0;
+    mostrar(grelha, sz);
 
-    while(testa==1)
+    while(jogada=(int)aux)
     {
-        scanf("%d", &testa);
-        printf("baixo\n");
-        baixo(grelha, sz);
-        mostrar(grelha, sz);
-        printf("novo numero\n");
-        novonumero(grelha, sz);
-        mostrar(grelha, sz);
-        printf("direita\n");
-        direita(grelha,sz);
-        mostrar(grelha, sz);
+        scanf("%c", &aux5);
+        jogada = (int)aux5;
+        if (jogada==(int)aux)
+        {
+            numero_de_comb = cima(grelha,sz);
+            novonumero(grelha,sz);
+            mostrar(grelha, sz);
+            printf("Numero de combinacoes: %d\n", numero_de_comb);
+        }
+           if (jogada==(int)aux3)
+        {
+            numero_de_comb = baixo(grelha,sz);
+            novonumero(grelha,sz);
+            mostrar(grelha, sz);
+            printf("Numero de combinacoes: %d\n", numero_de_comb);
+        }
+               if (jogada==(int)aux2)
+        {
+            numero_de_comb = direita(grelha,sz);
+            novonumero(grelha,sz);
+            mostrar(grelha, sz);
+            printf("Numero de combinacoes: %d\n", numero_de_comb);
+        }
+             if (jogada==(int)aux4)
+        {
+            numero_de_comb = esquerda(grelha,sz);
+            novonumero(grelha,sz);
+            mostrar(grelha, sz);
+            printf("Numero de combinacoes: %d\n", numero_de_comb);
+        }
+
     }
 
 
